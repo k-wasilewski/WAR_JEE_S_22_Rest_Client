7b8158a4a8d7647d6466b290abceda302038002b
Kuba <k.k.wasilewski@gmail.com>
2020-01-04 16:01:47 +0100

    done

--- src/main/java/pl/coderslab/app/js/scripts.js
+++ src/main/java/pl/coderslab/app/js/scripts.js
@@ -1,27 +1,68 @@
 $(function a() {
 
+    //one ajax call
+
+    var do_ajax = function() {
+
+        if ($('#books').data('ajax')=='get') {
+            var jsonGet = null;
+            $.ajax({
+                'async': false,
+                'global': false,
+                'url': 'http://localhost:8282/books/',
+                'dataType': "json",
+                'success': function (data) {
+                    jsonGet = data;
+                }
+            });
+            return jsonGet;
+        }
+
+        $.each($('tr'), function () {
+            if ($(this).data('ajax')=='del') {
+                del($(this).attr('id'));
+                $(this).remove();
+                return;
+            }
+        })
+
+        if ($('#submit').data('ajax')=='post') {
+            var data = {
+                'isbn': $("#isbn").val(),
+                'title': $("#title").val(),
+                'author': $("#author").val(),
+                'publisher': $("#publisher").val(),
+                'type': $("#type").val()
+            }
+            $.ajax({
+                url: 'http://localhost:8282/books/',
+                type: 'post',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                dataType: 'json',
+                data: JSON.stringify(data),
+                success: function () {
+                    location.reload();
+                }
+            });
+        }
+    }
+
+    //-------------------------------------------------------------------------
+
     //get all books
 
     var books;
-    var jsonGet = (function () {
-        var jsonGet = null;
-        $.ajax({
-            'async': false,
-            'global': false,
-            'url': 'http://localhost:8282/books/',
-            'dataType': "json",
-            'success': function (data) {
-                jsonGet = data;
-            }
-        });
-        return jsonGet;
-    })();
+    $('#books').data('ajax', 'get');
+    var jsonGet = do_ajax();
+    $('#books').data('ajax', null);
     books=Array.from(jsonGet);
     var books2=[];
     books.forEach(function (value, index) {
         books2.push( [value.id, value.isbn, value.title, value.author, value.publisher, value.type]);
     });
-    //console.log(books2);
 
     //------------------------------------------------------------------
 
@@ -32,29 +73,27 @@ $(function a() {
             url: "http://localhost:8282/books/" + id,
             type: "DELETE",
         });
-        setTimeout(function () {
-            location.reload();
-        }, 500);
     }
 
 
     //-----------------------------------------------------------------------
 
-    //create the all-books table struct
+    //create the all-books table
 
     books2.forEach(function (value, index, array) {
         var row = $('<tr>');
+        row.attr('id', value[0]);
+
         row.addClass('table-active');
         var title = $('<td>');
         title.text(value[2]);
         var div = $('<div>');
-        div.text('siema');
         div.css('display', 'none');
         title.append(div);
-        title.click(function (e) {
+        title.click(function (e) {  //details
             e.preventDefault();
             e.stopPropagation();
-            if (div.css('display')=='none') {   //details
+            if (div.css('display')=='none') {
                 div.html("<span>"+"<br>"+
                     "Id: "+value[0]+"<br>"+
                         "ISBN: "+value[1]+"<br>"+
@@ -76,15 +115,25 @@ $(function a() {
         row.append(btnTd);
         $('#books').append(row);
         var $this = $(this);
-        btn.click(function (e) {
+        btn.click(function (e) {    //delete btn
             e.preventDefault();
             e.stopPropagation();
-            del(value[0]);    //delete row from serv
+            row.data('ajax', 'del');
+            do_ajax();
             return false;
         });
     })
 
     //-----------------------------------------------------------------
 
+    //adding a new book
+
+    $('#submit').click(function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        $('#submit').data('ajax', 'post');
+        do_ajax();
+        return false;
+    })
 
 });
